8e176d20c918598b34bfe7cd9ee1ace278f4605f
PMichnal <110689550+PMichnal@users.noreply.github.com>
2024-02-04 23:18:16 +0100

    Update wyzwalacze.sql
    dodałem dwa triggery (te z hp)

--- wyzwalacze.sql
+++ wyzwalacze.sql
@@ -57,3 +57,28 @@ BEGIN
 	DELETE FROM equipment WHERE weapon_id IN (SELECT weapon_id FROM DELETED);
 END
 GO;
+
+CREATE TRIGGER check_hp
+ON entities
+AFTER UPDATE
+AS
+BEGIN
+    IF (SELECT current_hp FROM inserted) <= 0
+    BEGIN
+        UPDATE entities
+        SET alive = 1
+        WHERE entity_id IN (SELECT entity_id FROM inserted);
+    END
+END;
+GO;
+
+CREATE TRIGGER check_hp
+ON entities
+AFTER UPDATE
+AS
+BEGIN
+	UPDATE entities
+	SET current_hp = max_hp
+	WHERE entity_id IN (SELECT entity_id FROM inserted where current_hp > max_hp);
+END;
+GO;
