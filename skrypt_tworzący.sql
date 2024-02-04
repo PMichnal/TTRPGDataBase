9fb8babfd5cbf98ff835617532cb854ca56f4c00
Paula Konowalska <134429504+poczujluz@users.noreply.github.com>
2024-02-04 23:50:55 +0100

    Update skrypt_tworzący.sql

--- skrypt_tworzący.sql
+++ skrypt_tworzący.sql
@@ -1,14 +1,17 @@
 CREATE DATABASE [dnd]
 
+
 CREATE TABLE [entities] (
   [entity_id] integer PRIMARY KEY,
-  [aligment_id] integer NOT NULL,
+  [alignment_id] integer NOT NULL,
   [type] nvarchar(255) NOT NULL CHECK ([type] IN ('pc', 'npc', 'monster')),
   [initiative_bonus] integer NOT NULL,
+  [exp] integer NOT NULL,
   [size] nvarchar(255) NOT NULL,
   [name] nvarchar(255) NOT NULL,
   [ac] integer NOT NULL,
-  [hp] integer NOT NULL,
+  [max_hp] integer NOT NULL,
+  [current_hp] NOT NULL,
   [str] integer NOT NULL,
   [dex] integer NOT NULL,
   [con] integer NOT NULL,
@@ -26,134 +29,136 @@ CREATE TABLE [equipment] (
 )
 GO
 
-CREATE TABLE [aligment] (
-  [aligment_id] integer PRIMARY KEY,
+CREATE TABLE [alignment] (
+  [alignment_id] integer PRIMARY KEY,
   [x_axis] nvarchar(255) NOT NULL CHECK ([x_axis] IN ('chaotic', 'neutral', 'lawful')),
   [y_axis] nvarchar(255) NOT NULL CHECK ([y_axis] IN ('good', 'neutral', 'evil'))
 )
 GO
 
 CREATE TABLE [monsters] (
-  [entity_id] integer PRIMARY KEY,
-  [exp] integer NOT NULL,
-  [type] nvarchar(255) NOT NULL CHECK ([type] IN ('aberration', 'beast', 'celestial', 'construct', 'dragon', 'elemental', 'fey', 'fiend', 'giant', 'monstrolity', 'ooze', 'plant', 'undead'))
+	[entity_id] integer PRIMARY KEY,
+	[type] nvarchar(255) NOT NULL CHECK ([type] IN ('aberration', 'beast', 'celestial', 'construct', 'dragon', 'elemental', 'fey', 'fiend', 'giant', 'monstrolity', 'ooze', 'plant', 'undead')),
+	[race] nvarchar(255) NOT NULL
 )
 GO
 
 CREATE TABLE [entity_skills] (
-  [entity_id] integer,
-  [skill_id] integer,
-  PRIMARY KEY ([entity_id], [skill_id])
+	[entity_id] integer,
+	[skill_id] integer,
+	PRIMARY KEY ([entity_id], [skill_id])
 )
 GO
 
 CREATE TABLE [skills] (
-  [skill_id] integer PRIMARY KEY,
-  [name] nvarchar(255) UNIQUE NOT NULL,
-  [stat] nvarchar(255) NOT NULL CHECK ([stat] IN ('charisma', 'dexterity', 'constitution', 'inteligence', 'wisdom', 'strength'))
+	[skill_id] integer PRIMARY KEY,
+	[name] nvarchar(255) UNIQUE NOT NULL,
+	[stat] nvarchar(255) NOT NULL CHECK ([stat] IN ('charisma', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'strength'))
 )
 GO
 
 CREATE TABLE [npc] (
-  [entity_id] integer PRIMARY KEY,
-  [exp] integer NOT NULL,
-  [race] nvarchar(255) NOT NULL
+	[entity_id] integer PRIMARY KEY,
+	[race] nvarchar(255) NOT NULL
 )
 GO
 
 CREATE TABLE [pc] (
-  [entity_id] integer,
-  [pc_id] integer,
-  [class] nvarchar(255) NOT NULL,
-  [subclass] nvarchar(255) NOT NULL,
-  [background] nvarchar(255) NOT NULL,
-  PRIMARY KEY ([pc_id])
+	[entity_id] integer,
+	[pc_id] integer,
+	[race] nvarchar(255) NOT NULL,
+	[class] nvarchar(255) NOT NULL,
+	[subclass] nvarchar(255) NOT NULL,
+	[background] nvarchar(255) NOT NULL,
+	PRIMARY KEY ([pc_id])
 )
 GO
 
 CREATE TABLE [pc_feats] (
-  [pc_id] integer,
-  [feat_id] integer,
-  PRIMARY KEY ([pc_id], [feat_id])
+	[pc_id] integer,
+	[feat_id] integer,
+	PRIMARY KEY ([pc_id], [feat_id])
 )
 GO
 
 CREATE TABLE [feats] (
-  [feat_id] integer PRIMARY KEY,
-  [name] nvarchar(255) NOT NULL,
-  [description] nvarchar(255) NOT NULL
+	[feat_id] integer PRIMARY KEY,
+	[name] nvarchar(255) NOT NULL,
+	[description] nvarchar(255) NOT NULL
 )
 GO
 
 CREATE TABLE [proficiences] (
-  [proficiency_id] integer PRIMARY KEY,
-  [name] nvarchar(255) NOT NULL
+	[proficiency_id] integer PRIMARY KEY,
+	[name] nvarchar(255) NOT NULL
 )
 GO
 
 CREATE TABLE [entity_proficiences] (
-  [entity_id] integer,
-  [proficiency_id] integer,
-  PRIMARY KEY ([entity_id], [proficiency_id])
+	[entity_id] integer,
+	[proficiency_id] integer,
+	PRIMARY KEY ([entity_id], [proficiency_id])
 )
 GO
 
 CREATE TABLE [weapon_types] (
-  [type_id] integer,
-  [name] nvarchar(255) NOT NULL,
-  PRIMARY KEY ([type_id])
+	[type_id] integer,
+	[name] nvarchar(255) NOT NULL,
+	PRIMARY KEY ([type_id])
 )
 GO
 
 CREATE TABLE [weapon] (
-  [weapon_id] integer PRIMARY KEY,
-  [type_id] integer NOT NULL,
-  [name] integer NOT NULL,
-  [proficiency_id] integer,
-  [damage] nvarchar(255) NOT NULL,
-  [damage_type_id] integer NOT NULL,
-  [stat] nvarchar(255) NOT NULL
+	[weapon_id] integer PRIMARY KEY,
+	[type_id] integer NOT NULL,
+	[name] integer NOT NULL,
+	[proficiency_id] integer,
+	[damage] nvarchar(255) NOT NULL,
+	[damage_type_id] integer NOT NULL,
+	[stat] nvarchar(255) NOT NULL
 )
 GO
 
 CREATE TABLE [spells] (
-  [spell_id] integer PRIMARY KEY,
-  [spell_lvl] integer NOT NULL,
-  [name] nvarchar(255) NOT NULL,
-  [damage] nvarchar(255) NOT NULL,
-  [damege_type_id] integer NOT NULL,
-  [casting_time] integer NOT NULL,
-  [range] nvarchar(255) NOT NULL,
-  [area] bit NOT NULL,    -- bit to odpowiednik boolean, nie ma w sql server dedykowanego typu dla zmiennej typu boolowskiego
-  [duration] integer NOT NULL
+	[spell_id] integer PRIMARY KEY,
+	[spell_lvl] integer NOT NULL,
+	[name] nvarchar(255) NOT NULL,
+	[damage] nvarchar(255) NOT NULL,
+	[damege_type_id] integer NOT NULL,
+	[casting_time] integer NOT NULL,
+	[range] nvarchar(255) NOT NULL,
+	[area] bit NOT NULL,    -- bit to odpowiednik boolean, nie ma w sql server dedykowanego typu dla zmiennej typu boolowskiego
+	[duration] integer NOT NULL
 )
 GO
 
 CREATE TABLE [entity_spells_known] (
-  [entity_id] integer,
-  [spell_id] integer,
-  PRIMARY KEY ([entity_id], [spell_id])
+	[entity_id] integer,
+	[spell_id] integer,
+	PRIMARY KEY ([entity_id], [spell_id])
 )
 GO
 
 CREATE TABLE [entity_spellslots] (
-  [entity_id] integer,
-  [spell_lvl] integer,
-  [number] integer NOT NULL,
-  PRIMARY KEY ([entity_id], [spell_lvl])
+	[entity_id] integer,
+	[spell_lvl] integer,
+	[max_number] integer NOT NULL,
+  [current_number] integer NOT NULL,
+	PRIMARY KEY ([entity_id], [spell_lvl])
 )
 GO
 
 CREATE TABLE [damage_types] (
-  [damage_type_id] integer PRIMARY KEY,
-  [name] nvarchar(255) UNIQUE NOT NULL
+	[damage_type_id] integer PRIMARY KEY,
+	[name] nvarchar(255) UNIQUE NOT NULL
 )
 GO
+
 CREATE TABLE [entity_resistances_and_vulnerabilities] (
-  [damage_type_id] integer,
-  [entity_id] integer,
-  [type] nvarchar(255) NOT NULL,
-  PRIMARY KEY ([damage_type_id], [entity_id])
+	[damage_type_id] integer,
+	[entity_id] integer,
+	[type] nvarchar(255) NOT NULL,
+	PRIMARY KEY ([damage_type_id], [entity_id])
 )
 GO
 
@@ -172,7 +177,7 @@ GO
 ALTER TABLE [pc] ADD FOREIGN KEY ([entity_id]) REFERENCES [entities] ([entity_id])
 GO
 
-ALTER TABLE [entities] ADD FOREIGN KEY ([aligment_id]) REFERENCES [aligment] ([aligment_id])
+ALTER TABLE [entities] ADD FOREIGN KEY ([alignment_id]) REFERENCES [alignment] ([alignment_id])
 GO
 
 ALTER TABLE [entity_spellslots] ADD FOREIGN KEY ([entity_id]) REFERENCES [entities] ([entity_id])
